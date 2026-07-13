0868007bf78595f2f778b378f70a0c6d37556424
Lukas Drognitz <lukas.drognitz@avanade.com>
2026-07-13 18:19:08 +0200

    Normalize flash hard-words image to fixed result size

--- js/app.js
+++ js/app.js
@@ -3331,7 +3331,7 @@ function bindGlobalEvents() {
 function initServiceWorker() {
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-      .register("sw.js?v=159", { updateViaCache: "none" })
+      .register("sw.js?v=160", { updateViaCache: "none" })
       .then((registration) => registration.update())
       .catch(() => {
         // App should continue even if service worker update fails.
