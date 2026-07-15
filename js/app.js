6bbaa9828a0e92b11f67d407747ec78318ea0eb3
Lukas Drognitz <lukas.drognitz@avanade.com>
2026-07-15 18:17:11 +0200

    Upgrade avatar assets to high-detail character sheets

--- js/app.js
+++ js/app.js
@@ -389,7 +389,7 @@ const CONVERSATION_TOPICS = {
 const START_SCREEN_SESSION_KEY = "assamese-app-start-screen-seen";
 const LOVE_MILESTONE_STEP_XP = 2110;
 const LOVE_MILESTONE_MESSAGE = "Candles may fade and cake will be gone but my love for you burns brightly forever strong!";
-const APP_BUILD_VERSION = "20260715-170";
+const APP_BUILD_VERSION = "20260715-171";
 const XP_TO_RUPEE_RATE = 5;
 const CHEST_OPEN_ANIMATION_MS = 1050;
 
@@ -4288,7 +4288,7 @@ function bindGlobalEvents() {
 function initServiceWorker() {
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-      .register("sw.js?v=182", { updateViaCache: "none" })
+      .register("sw.js?v=183", { updateViaCache: "none" })
       .then((registration) => registration.update())
       .catch(() => {
         // App should continue even if service worker update fails.
--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "assamese-survival-dictionary-v182";
+const CACHE_NAME = "assamese-survival-dictionary-v183";
 
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./css/style.css?v=20260715-91",
-  "./js/app.js?v=20260715-170",
+  "./css/style.css?v=20260715-92",
+  "./js/app.js?v=20260715-171",
   "./js/dictionary.js?v=20260710-45",
   "./js/lessons.js?v=20260715-06",
   "./js/flashcards.js?v=20260713-38",
