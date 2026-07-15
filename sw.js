75ddbc2f9340d7d2ea38eb46795bbf0c347e851d
Lukas Drognitz <lukas.drognitz@avanade.com>
2026-07-15 16:29:26 +0200

    Add species-specific idle animations for avatar animals

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "assamese-survival-dictionary-v177";
+const CACHE_NAME = "assamese-survival-dictionary-v178";
 
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./css/style.css?v=20260715-86",
-  "./js/app.js?v=20260715-165",
+  "./css/style.css?v=20260715-87",
+  "./js/app.js?v=20260715-166",
   "./js/dictionary.js?v=20260710-45",
   "./js/lessons.js?v=20260715-05",
   "./js/flashcards.js?v=20260713-38",
