694de3ce25d4429ce452b01b060abdac0f5d3636
Lukas Drognitz <lukas.drognitz@avanade.com>
2026-07-16 10:56:42 +0200

    Replace Rhino classic image and refresh cache versions

--- js/app.js
+++ js/app.js
@@ -93,7 +93,7 @@ const LEGACY_AVATAR_MAP = {
 };
 
 const AVATAR_META_BY_ID = Object.fromEntries(AVATAR_REWARDS.map((item) => [item.value, item]));
-const AVATAR_IMAGE_VERSION = "20260716-199";
+const AVATAR_IMAGE_VERSION = "20260716-200";
 const PEACOCK_OUTFIT_OPTIONS = [
   { value: "classic", label: "Classic" },
   { value: "professor", label: "Professor" }
@@ -4310,7 +4310,7 @@ function bindGlobalEvents() {
 function initServiceWorker() {
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-      .register("sw.js?v=211", { updateViaCache: "none" })
+      .register("sw.js?v=212", { updateViaCache: "none" })
       .then((registration) => registration.update())
       .catch(() => {
         // App should continue even if service worker update fails.
